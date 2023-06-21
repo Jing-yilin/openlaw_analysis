887d400a4164342d6fb364604a7beace05f84ff4
Jing-yilin <1154896650@qq.com>
2023-06-21 22:51:36 +0800

    fix-bug: 更新加密算法

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,41 +1,53 @@
-from Crypto.Cipher import AES, PKCS1_v1_5
-from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad
-from Crypto.Protocol.KDF import PBKDF2
-import base64
 import random
 import string
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.PublicKey import RSA
+from base64 import b64encode
+from Crypto.Util.Padding import pad
+import math
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_pkcs1_v1_5
 
-public_key = '''-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0zI8aibR9ZN57QObFxvI
-wiRTmELItVVBLMrLd71ZqakR6oWUKkcAGgmxad2TCy3UeRe4A0Dduw97oXlbl5rK
-RGISzpLO8iMSYtsim5aXZX9SB5x3S9ees4CZ6MYD/4XQOTrU0r1TMT6wXlhVvwNb
-fMNYHm3vkY0rhfxBCVPFJoHjAGDFWNCAhf4KfalfvWsGL32p8N/exG2S4yXVHuV6
-cHDyFJAItKVmyuTmB62pnPs5KvNv6oPmtmhMxxsvBOyh7uLwB5TonxtZpWZ3A1wf
-43ByuU7F3qGnFqL0GeG/JuK+ZR40LARyevHy9OZ5pMa0Nwqb8PwfK810Bc8PxD8N
-EwIDAQAB
------END PUBLIC KEY-----'''
+public_key = open('public.pem', 'r').read()
 
 encrypt_pass_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz*&-%/!?*+=()"
 
-def key_encrypt(data):
-    pass_phrase = generate_encrypt_password(32).encode('utf-8')
-    iv = get_random_bytes(16)
-    salt = get_random_bytes(16)
+
+rsakey = RSA.import_key(public_key)
+def key_encrypt(data:str):
+    # 生成随机的加密密钥
+    pass_phrase = generate_encrypt_password(32)
+    # 生成随机的iv
+    iv = bytes.fromhex(''.join(random.choices(string.hexdigits, k=32)))
+    # 生成随机的salt
+    salt = bytes.fromhex(''.join(random.choices(string.hexdigits, k=32)))
+
+    # 使用PBKDF2算法生成加密密钥
     key = PBKDF2(pass_phrase, salt, dkLen=16, count=1000)
 
-    aes_cipher = AES.new(key, AES.MODE_CBC, iv)
-    encrypted_message = aes_cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
+    # 使用生成的密钥和iv，使用AES算法对数据data进行加密
+    data = pad(data.encode('utf-8'), AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encrypted_data = b64encode(cipher.encrypt(data)).decode('utf-8')
 
-    aes_key = pass_phrase.hex() + ":::" + salt.hex() + ":::" + iv.hex()
-    rsa_public_key = RSA.import_key(public_key)
-    cipher_rsa = PKCS1_v1_5.new(rsa_public_key)
-    encrypted_key = cipher_rsa.encrypt(aes_key.encode())
+    # 将密码、salt和iv拼接成字符串
+    aes_key = pass_phrase + ":::" + salt.hex() + ":::" + iv.hex()
 
-    encrypted = base64.b64encode(encrypted_key).decode() + ":::" + base64.b64encode(encrypted_message).decode()
+    # 将加密后的数据转换为Base64格式
+    encrypted_message = encrypted_data
+
+    # 使用RSA公钥加密AES密钥
+    cipher = Cipher_pkcs1_v1_5.new(rsakey)
+    encrypted_key = b64encode(cipher.encrypt(aes_key.encode('utf-8')))
+
+    # 将加密的密钥和加密后的数据拼接成字符串并返回
+    encrypted = encrypted_key.decode('utf-8') + ":::" + encrypted_message
     return encrypted
 
+
 def generate_encrypt_password(length):
-    randomstring = ''.join(random.choice(encrypt_pass_chars) for _ in range(length))
-    return randomstring
+    random_string = ''
+    for i in range(length):
+        rnum = math.floor(random.random() * len(encrypt_pass_chars))
+        random_string += encrypt_pass_chars[rnum:rnum+1]
+    return random_string
