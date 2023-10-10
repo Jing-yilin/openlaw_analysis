7eb676eb03abb2ff03d5dd2e8a68db4cc1d9a51a
Jing-yilin <yilin.jing.ai@outlook.com>
2023-10-10 14:31:02 +0800

    feat: 使用异步爬虫

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import asyncio
+import aiohttp
+from codetiming import Timer
 from src.spider import OpenLawSpider
 from src.analysis import Analyzer
 from src.utils import read_config
 
-def main():
+
+async def main():
     # 读取配置文件
     config = read_config()
     cookie = config["cookie"]
@@ -11,18 +15,30 @@ def main():
     strat_page = config["strat_page"]
     end_page = config["end_page"]
 
-    # 数据爬取
-    spider = OpenLawSpider(keyword, strat_page, end_page, cookie, user_agent)
-    # 1. 爬取链接,保存为csv文件
-    if spider.crawl_links(): # 如果爬取成功
-        # 2. 根据链接，爬取内容,保存为json文件
-        spider.crawl_contents()
-        # 3. 把json文件转换成excel文件，以便于用户查看
-        # spider.save_to_excel()
+    with Timer(text="总耗时: {:.1f} s"):
+        # 创建session
+        async with aiohttp.ClientSession() as session:
+            # 数据爬取
+            spider = OpenLawSpider(
+                keyword,
+                strat_page,
+                end_page,
+                cookie,
+                user_agent,
+                session=session,
+                concurrent=10
+            )
+            # 1. 爬取链接,保存为csv文件
+            if await spider.crawl_links():  # 如果爬取成功
+                # 2. 根据链接，爬取内容,保存为json文件
+                await spider.crawl_contents()
+                # 3. 把json文件转换成excel文件，以便于用户查看
+                # spider.save_to_excel()
 
-        # 数据分析
-        analyzer = Analyzer(keyword)
-        analyzer.auto_analysis()
+                # 数据分析
+                analyzer = Analyzer(keyword)
+                analyzer.auto_analysis()
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- src/spider/openlaw_spider.py
+++ src/spider/openlaw_spider.py
@@ -4,18 +4,29 @@ import time
 import tqdm
 import json
 import pandas as pd
+import asyncio
+import aiohttp
 import yaml
 import os
 import sys
 import pathlib
+from codetiming import Timer
 
 from ..utils import create_dir
 
 
-
 class OpenLawSpider:
     def __init__(
-        self, keyword, strat_page, end_page, cookie, user_agent, links={}, contents=[]
+        self,
+        keyword: str,
+        strat_page: int,
+        end_page: int,
+        cookie: str,
+        user_agent: str,
+        links: dict = {},
+        contents: list = [],
+        session: aiohttp.ClientSession = None,
+        concurrent: int = 10,
     ):
         self.base_url = "http://openlaw.cn"
         self.keyword = keyword
@@ -30,21 +41,30 @@ class OpenLawSpider:
         }
         self.links = links
         self.contents = contents
+        self.session = session
+        self.concurrent = concurrent
         create_dir(self.save_dir)
 
-    def get_url(self, page: str) -> list:
-        keyword_ = requests.utils.quote(self.keyword)
-        url = f"{self.base_url}/search/judgement/advanced?showResults=true&keyword={keyword_}&causeId=&caseNo=&litigationType=&docType=&litigant=&plaintiff=&defendant=&thirdParty=&lawyerId=&lawFirmId=&legals=&courtId=&judgeId=&clerk=&judgeDateYear=&judgeDateBegin=&judgeDateEnd=&zone=&procedureType=&lawId=&lawSearch=&courtLevel=&judgeResult=&wordCount=&page={page}"
-        text = requests.get(url, headers=self.headers).text
-        # 把keyword中文转换成url编码
-        # 1. 创建正则表达式对象
-        pattern = re.compile(r"/judgement/[0-9a-z]*\?keyword=" + keyword_)
-        # 2. 使用正则表达式对象匹配text
-        link_result = pattern.findall(text)
-        link_result = [self.base_url + link for link in link_result]
-        return link_result
+    async def get_url(
+        self, target_page_queue: asyncio.Queue, session: aiohttp.ClientSession
+    ) -> list:
+        while not target_page_queue.empty():
+            page = await target_page_queue.get()
+            keyword_ = requests.utils.quote(self.keyword)
+            url = f"{self.base_url}/search/judgement/advanced?showResults=true&keyword={keyword_}&causeId=&caseNo=&litigationType=&docType=&litigant=&plaintiff=&defendant=&thirdParty=&lawyerId=&lawFirmId=&legals=&courtId=&judgeId=&clerk=&judgeDateYear=&judgeDateBegin=&judgeDateEnd=&zone=&procedureType=&lawId=&lawSearch=&courtLevel=&judgeResult=&wordCount=&page={page}"
+            print(f"\n- 正在爬取第{page}页: {url}")
+            async with session.get(url, headers=self.headers) as response:
+                text = await response.text()
+            # 把keyword中文转换成url编码
+            # 1. 创建正则表达式对象
+            pattern = re.compile(r"/judgement/[0-9a-z]*\?keyword=" + keyword_)
+            # 2. 使用正则表达式对象匹配text
+            link_result = pattern.findall(text)
+            link_result = [self.base_url + link for link in link_result]
+            if link_result:
+                self.links[page] = link_result
 
-    def crawl_links(self) -> bool:
+    async def crawl_links(self) -> bool:
         # 错误处理
         if self.strat_page > self.end_page:
             print(f"起始页{self.strat_page}大于结束页{self.end_page}!!!")
@@ -72,19 +92,16 @@ class OpenLawSpider:
             print(f"======所有链接已经爬取过了======")
             return True
 
+        target_page_queue = asyncio.Queue()
+        for page in target_pages:
+            await target_page_queue.put(page)
+
         print(f"======开始爬取链接======")
-        for page in tqdm.tqdm(target_pages):
-            link_result = self.get_url(page)
-            if link_result is None or len(link_result) == 0:
-                print(f"第{page}页没有爬取到链接!!!")
-                continue
-            else:
-                # 同时记录page和link_result
-                self.links[page] = link_result
-                links[page] = link_result
-                # 睡眠5秒
-                time.sleep(2)
-                print("睡眠2秒")
+
+        tasks = []
+        for i in range(self.concurrent):
+            tasks.append(self.get_url(target_page_queue, self.session))
+        await asyncio.gather(*tasks)
         print(f"======链接爬取完结束=====")
 
         if self.links is None or len(self.links.keys()) == 0:
@@ -117,57 +134,64 @@ class OpenLawSpider:
             text = re.sub(r"&nbsp;", "", text)
             return text
 
-    def get_content(self, url: str) -> dict:
-        content = {}
-        text = requests.get(url, headers=self.headers).text
-        content["url"] = url
-        content["title"] = self.filter_text(
-            re.findall('<h2 class="entry-title">(.*)</h2>', text)
-        )
-        content["case_number"] = self.filter_text(
-            re.findall('<li class="clearfix ht-kb-pf-standard">案号：(.*)</li>', text)
-        )
-        content["court"] = self.filter_text(
-            re.findall('<li class="clearfix ht-kb-pf-standard">法院：(.*)</li>', text)
-        )
-        content["data"] = self.filter_text(
-            re.findall('<li class="clearfix ht-kb-pf-standard">时间：(.*)</li>', text)
-        )
-        content["cause"] = self.filter_text(
-            re.findall('<li class="clearfix ht-kb-pf-standard">案由：(.*)</li>', text)
-        )
-        content["type"] = self.filter_text(
-            re.findall('<li class="clearfix ht-kb-pf-standard">类型：(.*)</li>', text)
-        )
-        content["procedure"] = self.filter_text(
-            re.findall('<li class="clearfix ht-kb-pf-standard">程序：(.*)</li>', text)
-        )
-        content["procedure_explain"] = self.filter_text(
-            re.findall(
-                '<div class="part" id="Explain">\s*<!--.*?-->\s*<p>(.*?)<\/p>\s*<\/div>',
-                text,
+    async def add_content(
+        self, url_queue: asyncio.Queue, session: aiohttp.ClientSession
+    ) -> None:
+        while not url_queue.empty():
+            content = {}
+            url = await url_queue.get()
+            print(f"\n- 正在爬取: {url}")
+            async with session.get(url, headers=self.headers) as response:
+                text = await response.text()
+            content["url"] = url
+            content["title"] = self.filter_text(
+                re.findall('<h2 class="entry-title">(.*)</h2>', text)
             )
-        )
-        content["tags"] = (
-            re.findall('<a href="[^"]+" rel="tag">(.*?)</a>', text)
-            if re.findall('<a href="[^"]+" rel="tag">(.*?)</a>', text)
-            else ""
-        )
-        content["opinion"] = self.filter_text(
-            re.findall(
-                '<div class="part" id="Opinion">\s*<!--.*?-->\s*<p>(.*?)<\/p>\s*<\/div>',
-                text,
+            content["case_number"] = self.filter_text(
+                re.findall('<li class="clearfix ht-kb-pf-standard">案号：(.*)</li>', text)
             )
-        )
-        content["verdict"] = self.filter_text(
-            re.findall(
-                '<div class="part" id="Verdict">\s*<!--.*?-->\s*<p>(.*?)<\/p>\s*<\/div>',
-                text,
+            content["court"] = self.filter_text(
+                re.findall('<li class="clearfix ht-kb-pf-standard">法院：(.*)</li>', text)
             )
-        )
-        return content
+            content["data"] = self.filter_text(
+                re.findall('<li class="clearfix ht-kb-pf-standard">时间：(.*)</li>', text)
+            )
+            content["cause"] = self.filter_text(
+                re.findall('<li class="clearfix ht-kb-pf-standard">案由：(.*)</li>', text)
+            )
+            content["type"] = self.filter_text(
+                re.findall('<li class="clearfix ht-kb-pf-standard">类型：(.*)</li>', text)
+            )
+            content["procedure"] = self.filter_text(
+                re.findall('<li class="clearfix ht-kb-pf-standard">程序：(.*)</li>', text)
+            )
+            content["procedure_explain"] = self.filter_text(
+                re.findall(
+                    '<div class="part" id="Explain">\s*<!--.*?-->\s*<p>(.*?)<\/p>\s*<\/div>',
+                    text,
+                )
+            )
+            content["tags"] = (
+                re.findall('<a href="[^"]+" rel="tag">(.*?)</a>', text)
+                if re.findall('<a href="[^"]+" rel="tag">(.*?)</a>', text)
+                else ""
+            )
+            content["opinion"] = self.filter_text(
+                re.findall(
+                    '<div class="part" id="Opinion">\s*<!--.*?-->\s*<p>(.*?)<\/p>\s*<\/div>',
+                    text,
+                )
+            )
+            content["verdict"] = self.filter_text(
+                re.findall(
+                    '<div class="part" id="Verdict">\s*<!--.*?-->\s*<p>(.*?)<\/p>\s*<\/div>',
+                    text,
+                )
+            )
+            self.contents.append(content)
+            # asyncio.sleep(0.5)
 
-    def crawl_contents(self):
+    async def crawl_contents(self):
         contents = []
         urls = []  # 已经爬取过的链接
         file_name = f"{self.save_dir}/{self.keyword}_contents.json"
@@ -186,19 +210,23 @@ class OpenLawSpider:
                 if row["url"] not in target_urls:
                     self.contents.remove(row)
 
-        for link in tqdm.tqdm(target_urls):
-            if link in urls:
-                print(f"链接:{link} 已经爬取过")
-                continue
-            print(f"正在爬取内容:{link}")
-            content = self.get_content(link)
-            self.contents.append(content)
-            contents.append(content)
-            time.sleep(2)
+        # 创建队列
+        url_queue = asyncio.Queue()
+        for url in target_urls:
+            if url not in urls:
+                await url_queue.put(url)
+
+        print(f"======开始创建爬取任务======")
+        tasks = []
+        for i in range(self.concurrent):
+            tasks.append(self.add_content(url_queue, self.session))
+
+        await asyncio.gather(*tasks)
+
         print(f"======内容爬取完成======")
         # 保存
         with open(file_name, "w", encoding="utf-8") as f:
-            json.dump(contents, f, ensure_ascii=False)
+            json.dump(self.contents, f, ensure_ascii=False)
         print(f"======{file_name}保存完成======")
 
     def save_to_excel(self):
